909c7dcd1f4fb410fa81b8807c84662f650da99b
mateuslph <mlptche@gmail.com>
2024-01-14 18:04:39 -0300

    Projeto inicializado

---
+++ src/main/java/com/umprogramax/lojaStock/LojaStockApplication.java
@@ -0,0 +1,13 @@
+package com.umprogramax.lojaStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaStockApplication.class, args);
+	}
+
+}
