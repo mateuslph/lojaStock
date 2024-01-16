db446a1021e7c7a7b1da8f89c9932ec41a96a704
mateuslph <mlptche@gmail.com>
2024-01-16 20:48:20 -0300

    model Funcionario concluido

--- src/main/java/com/umprogramax/lojaStock/Model/Funcionario.java
+++ src/main/java/com/umprogramax/lojaStock/Model/Funcionario.java
@@ -1,4 +1,24 @@
 package com.umprogramax.lojaStock.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "funcionario")
+@Entity(name = "funcionario")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String nome;
+    private Integer cpf;
+    private String registro;
+    private String tipo;
+    private Endereco endereco;
+
 }
