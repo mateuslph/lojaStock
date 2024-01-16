3b2c9927d8d2c4c2f160ca4240adbb7e1a8f3697
mateuslph <mlptche@gmail.com>
2024-01-16 20:40:51 -0300

    model Produto concluida

--- src/main/java/com/umprogramax/lojaStock/Model/Produto.java
+++ src/main/java/com/umprogramax/lojaStock/Model/Produto.java
@@ -1,4 +1,22 @@
 package com.umprogramax.lojaStock.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "produto")
+@Entity(name = "produto")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String descricao;
+    private Float preco;
+    private String observacao;
+
 }
