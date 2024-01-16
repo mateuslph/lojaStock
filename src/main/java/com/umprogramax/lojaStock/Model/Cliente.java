96d3084c5b8c3d1a974fe08d89b3b4efe0305312
mateuslph <mlptche@gmail.com>
2024-01-16 20:53:03 -0300

    model Cliente pronto

--- src/main/java/com/umprogramax/lojaStock/Model/Cliente.java
+++ src/main/java/com/umprogramax/lojaStock/Model/Cliente.java
@@ -1,4 +1,26 @@
 package com.umprogramax.lojaStock.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Table(name = "cliente")
+@Entity(name = "cliente")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String nome;
+    private Integer cpf;
+    private LocalDate dataInscricao;
+    private String genero;
+    private Endereco endereco;
+
 }
