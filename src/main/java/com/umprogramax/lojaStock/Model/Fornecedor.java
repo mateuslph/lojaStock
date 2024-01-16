5b0670705f31ea74484b2deea5f2310f1e4fa8fc
mateuslph <mlptche@gmail.com>
2024-01-16 20:34:40 -0300

    model Fornecedor concluido

--- src/main/java/com/umprogramax/lojaStock/Model/Fornecedor.java
+++ src/main/java/com/umprogramax/lojaStock/Model/Fornecedor.java
@@ -1,4 +1,25 @@
 package com.umprogramax.lojaStock.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Table(name="fornecedor")
+@Entity(name="fornecedor")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String nomeFantazia;
+    private Integer cnpj;
+    private LocalDate dataInscricao;
+    private Endereco endereco;
+
 }
