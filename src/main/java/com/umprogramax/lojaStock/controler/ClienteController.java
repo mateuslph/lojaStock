8fbb5be6539919d1dcd9c2a8540c597a2e74b26e
mateuslph <mlptche@gmail.com>
2024-01-18 20:13:43 -0300

    add anotacao nos controllers

--- src/main/java/com/umprogramax/lojaStock/controler/ClienteController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/ClienteController.java
@@ -1,4 +1,11 @@
 package com.umprogramax.lojaStock.controler;
 
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ClienteController {
+
+
+
 }
--- src/main/java/com/umprogramax/lojaStock/controler/ProdutoController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/ProdutoController.java
@@ -1,4 +1,7 @@
 package com.umprogramax.lojaStock.controler;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ProdutoController {
 }
