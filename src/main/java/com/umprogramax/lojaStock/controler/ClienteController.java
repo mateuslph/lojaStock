6f0bd8a1aa797b1d5725e0a5773675b73c57b401
mateuslph <mlptche@gmail.com>
2024-01-18 20:16:56 -0300

    add anotacao nos services

--- src/main/java/com/umprogramax/lojaStock/controler/ClienteController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/ClienteController.java
@@ -5,7 +5,4 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ClienteController {
-
-
-
 }
---
+++ src/main/java/com/umprogramax/lojaStock/service/ClienteService.java
@@ -0,0 +1,7 @@
+package com.umprogramax.lojaStock.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+}
