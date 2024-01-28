4923a50c314b255b553defb3a76160476d1ebd39
mateuslph <mlptche@gmail.com>
2024-01-28 15:01:21 -0300

    corrigido a rota de funcionario

--- src/main/java/com/umprogramax/lojaStock/controler/FuncionarioController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/FuncionarioController.java
@@ -19,17 +19,17 @@ public class FuncionarioController {
     @PostMapping(value = "/salvar-funcionario")
     public String save(@ModelAttribute("funcionario")Funcionario funcionario) {
         service.create(funcionario);
-        return "redirect:/salvar-funcionario/index";
+        return "redirect:/listar-funcionarios/index";
     }
 
-    @GetMapping(value="/listar-funcionario")
+    @GetMapping(value="/listar-funcionarios")
     public String index(Model model) {
         model.addAttribute("funcionarios", service.list());
         return "/listar-funcionarios/index";
     }
 
-    @PutMapping(value="/atualizar-funcionarios/{id}")
-    public String update(@PathVariable UUID id, @ModelAttribute("funcionarios") Funcionario funcionario, Model model) {
+    @PutMapping(value="/atualizar-funcionario/{id}")
+    public String update(@PathVariable UUID id, @ModelAttribute("funcionario") Funcionario funcionario, Model model) {
         Funcionario obj = service.charge(id);
         obj.setId(String.valueOf(id));
         obj.setNome(funcionario.getNome());
@@ -38,12 +38,12 @@ public class FuncionarioController {
         obj.setTipo(funcionario.getTipo());
         obj.setEndereco(funcionario.getEndereco());
         service.update(obj);
-        return "redirect:/atualizar-funcionario/index";
+        return "redirect:/listar-funcionarios/index";
     }
 
     public String delete(@PathVariable UUID id) {
         service.delete(id);
-        return "redirect:/listar-funcionario/index";
+        return "redirect:/listar-funcionarios/index";
     }
 
 }
