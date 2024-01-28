062dbf0d0aa3d664270c435122f6954d20f4a073
mateuslph <mlptche@gmail.com>
2024-01-27 21:05:09 -0300

    rota endereco completa

--- src/main/java/com/umprogramax/lojaStock/controler/EnderecoController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/EnderecoController.java
@@ -1,7 +1,49 @@
 package com.umprogramax.lojaStock.controler;
 
+import com.umprogramax.lojaStock.model.Cliente;
+import com.umprogramax.lojaStock.model.Endereco;
+import com.umprogramax.lojaStock.service.ClienteService;
+import com.umprogramax.lojaStock.service.EnderecoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 public class EnderecoController {
+
+    private EnderecoService service;
+
+    @PostMapping(value = "/salvar-endereco")
+    public String save(@ModelAttribute("endereco") Endereco endereco) {
+        service.create(endereco);
+        return "redirect:/salvar-endereco/index";
+    }
+
+    @GetMapping(value="/listar-enderecos")
+    public String index(Model model) {
+        model.addAttribute("enderecos", service.list());
+        return "/listar-enderecos/index";
+    }
+
+    @PutMapping(value="/atualizar-enderecos/{id}")
+    public String update(@PathVariable UUID id, @ModelAttribute("endereco") Endereco endereco, Model model) {
+        Endereco obj = service.charge(id);
+        obj.setId(String.valueOf(id));
+        obj.setPais(endereco.getPais());
+        obj.setUf(endereco.getUf());
+        obj.setMunicipio(endereco.getMunicipio());
+        obj.setLogradouro(endereco.getLogradouro());
+        obj.setComplemento(endereco.getComplemento());
+        obj.setCep(endereco.getCep());
+        service.update(obj);
+        return "redirect:/atualizar-enderecos/index";
+    }
+
+    public String delete(@PathVariable UUID id) {
+        service.delete(id);
+        return "redirect:/listar-enderecos/index";
+    }
+
 }
--- src/main/java/com/umprogramax/lojaStock/service/EnderecoService.java
+++ src/main/java/com/umprogramax/lojaStock/service/EnderecoService.java
@@ -1,7 +1,45 @@
 package com.umprogramax.lojaStock.service;
 
+import com.umprogramax.lojaStock.model.Cliente;
+import com.umprogramax.lojaStock.model.Endereco;
+import com.umprogramax.lojaStock.repository.ClienteRepository;
+import com.umprogramax.lojaStock.repository.EnderecoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EnderecoService {
+
+    @Autowired
+    EnderecoRepository repository;
+
+    public Endereco create(Endereco obj) {
+        obj.getPais();
+        obj.getUf();
+        obj.getMunicipio();
+        obj.getLogradouro();
+        obj.getComplemento();
+        obj.getCep();
+        return repository.save(obj);
+    }
+
+    public List<Endereco> list() {
+        return (List<Endereco>) repository.findAll();
+    }
+
+    public Endereco update(Endereco endereco) {
+        return repository.save(endereco);
+    }
+
+    public void delete(UUID id) {
+        repository.deleteById(id);
+    }
+
+    public Endereco charge(UUID id) {
+        return (Endereco) repository.findById(id).get();
+    }
+
 }
