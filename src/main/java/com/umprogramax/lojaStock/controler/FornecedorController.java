5859b06e289448d500abb52c13fa0e94943dc2c4
mateuslph <mlptche@gmail.com>
2024-01-27 21:16:46 -0300

    rota fornecedor completa

--- src/main/java/com/umprogramax/lojaStock/controler/FornecedorController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/FornecedorController.java
@@ -1,7 +1,48 @@
 package com.umprogramax.lojaStock.controler;
 
+import com.umprogramax.lojaStock.model.Endereco;
+import com.umprogramax.lojaStock.model.Fornecedor;
+import com.umprogramax.lojaStock.service.EnderecoService;
+import com.umprogramax.lojaStock.service.FornecedorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Controller
 public class FornecedorController {
+
+    private FornecedorService service;
+
+    @PostMapping(value = "/salvar-fornecedor")
+    public String save(@ModelAttribute("fornecedor")Fornecedor fornecedor) {
+        service.create(fornecedor);
+        return "redirect:/salvar-fornecedor/index";
+    }
+
+    @GetMapping(value="/listar-fornecedor")
+    public String index(Model model) {
+        model.addAttribute("fornecedores", service.list());
+        return "/listar-fornecedores/index";
+    }
+
+    @PutMapping(value="/atualizar-fornecedores/{id}")
+    public String update(@PathVariable UUID id, @ModelAttribute("fornecedor") Fornecedor fornecedor, Model model) {
+        Fornecedor obj = service.charge(id);
+        obj.setId(String.valueOf(id));
+        obj.setNomeFantazia(fornecedor.getNomeFantazia());
+        obj.setCnpj(fornecedor.getCnpj());
+        obj.setDataInscricao(fornecedor.getDataInscricao());
+        obj.setEndereco(fornecedor.getEndereco());
+        service.update(obj);
+        return "redirect:/atualizar-fornecedor/index";
+    }
+
+    public String delete(@PathVariable UUID id) {
+        service.delete(id);
+        return "redirect:/listar-fornecedores/index";
+    }
+
 }
--- src/main/java/com/umprogramax/lojaStock/service/FornecedorService.java
+++ src/main/java/com/umprogramax/lojaStock/service/FornecedorService.java
@@ -1,7 +1,44 @@
 package com.umprogramax.lojaStock.service;
 
+import com.umprogramax.lojaStock.model.Endereco;
+import com.umprogramax.lojaStock.model.Fornecedor;
+import com.umprogramax.lojaStock.repository.EnderecoRepository;
+import com.umprogramax.lojaStock.repository.FornecedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class FornecedorService {
+
+    @Autowired
+    FornecedorRepository repository;
+
+    public Fornecedor create(Fornecedor obj) {
+        obj.getNomeFantazia();
+        obj.getCnpj();
+        obj.getDataInscricao();
+        obj.getEndereco();
+        return repository.save(obj);
+    }
+
+    public List<Fornecedor> list() {
+        return (List<Fornecedor>) repository.findAll();
+    }
+
+    public Fornecedor update(Fornecedor fornecedor) {
+        return repository.save(fornecedor);
+    }
+
+    public void delete(UUID id) {
+        repository.deleteById(id);
+    }
+
+    public Fornecedor charge(UUID id) {
+        return (Fornecedor) repository.findById(id).get();
+    }
+
 }
