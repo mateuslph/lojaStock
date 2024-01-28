9f9aea179cee7c07282bb0ced813e85dc5576ce8
mateuslph <mlptche@gmail.com>
2024-01-28 15:05:49 -0300

    corrigido a rota de produto

--- src/main/java/com/umprogramax/lojaStock/controler/ProdutoController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/ProdutoController.java
@@ -18,24 +18,24 @@ public class ProdutoController {
     @PostMapping(value = "/salvar-produto")
     public String save(@ModelAttribute("produto")Produto produto) {
         service.create(produto);
-        return "redirect:/salvar-produto/index";
+        return "redirect:/listar-produtos/index";
     }
 
-    @GetMapping(value="/listar-produto")
+    @GetMapping(value="/listar-produtos")
     public String index(Model model) {
-        model.addAttribute("produto", service.list());
+        model.addAttribute("produtos", service.list());
         return "/listar-produtos/index";
     }
 
-    @PutMapping(value="/atualizar-produtos/{id}")
-    public String update(@PathVariable UUID id, @ModelAttribute("produtos") Produto produto, Model model) {
+    @PutMapping(value="/atualizar-produto/{id}")
+    public String update(@PathVariable UUID id, @ModelAttribute("produto") Produto produto, Model model) {
         Produto obj = service.charge(id);
         obj.setId(String.valueOf(id));
         obj.setDescricao(produto.getDescricao());
         obj.setPreco(produto.getPreco());
         obj.setObservacao(produto.getObservacao());
         service.update(obj);
-        return "redirect:/atualizar-produto/index";
+        return "redirect:/listar-produtos/index";
     }
 
     public String delete(@PathVariable UUID id) {
