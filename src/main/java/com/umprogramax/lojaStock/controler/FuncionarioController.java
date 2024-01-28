eab656f1f46c4d6c728178c5b833f7bc67540d9d
mateuslph <mlptche@gmail.com>
2024-01-27 21:27:31 -0300

    rota funcionario completa

--- src/main/java/com/umprogramax/lojaStock/controler/FuncionarioController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/FuncionarioController.java
@@ -1,7 +1,49 @@
 package com.umprogramax.lojaStock.controler;
 
+import com.umprogramax.lojaStock.model.Endereco;
+import com.umprogramax.lojaStock.model.Fornecedor;
+import com.umprogramax.lojaStock.model.Funcionario;
+import com.umprogramax.lojaStock.service.FornecedorService;
+import com.umprogramax.lojaStock.service.FuncionarioService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 public class FuncionarioController {
+
+    private FuncionarioService service;
+
+    @PostMapping(value = "/salvar-funcionario")
+    public String save(@ModelAttribute("funcionario")Funcionario funcionario) {
+        service.create(funcionario);
+        return "redirect:/salvar-funcionario/index";
+    }
+
+    @GetMapping(value="/listar-funcionario")
+    public String index(Model model) {
+        model.addAttribute("funcionarios", service.list());
+        return "/listar-funcionarios/index";
+    }
+
+    @PutMapping(value="/atualizar-funcionarios/{id}")
+    public String update(@PathVariable UUID id, @ModelAttribute("funcionarios") Funcionario funcionario, Model model) {
+        Funcionario obj = service.charge(id);
+        obj.setId(String.valueOf(id));
+        obj.setNome(funcionario.getNome());
+        obj.setCpf(funcionario.getCpf());
+        obj.setRegistro(funcionario.getRegistro());
+        obj.setTipo(funcionario.getTipo());
+        obj.setEndereco(funcionario.getEndereco());
+        service.update(obj);
+        return "redirect:/atualizar-funcionario/index";
+    }
+
+    public String delete(@PathVariable UUID id) {
+        service.delete(id);
+        return "redirect:/listar-funcionario/index";
+    }
+
 }
--- src/main/java/com/umprogramax/lojaStock/service/FuncionarioService.java
+++ src/main/java/com/umprogramax/lojaStock/service/FuncionarioService.java
@@ -1,7 +1,45 @@
 package com.umprogramax.lojaStock.service;
 
+import com.umprogramax.lojaStock.model.Endereco;
+import com.umprogramax.lojaStock.model.Fornecedor;
+import com.umprogramax.lojaStock.model.Funcionario;
+import com.umprogramax.lojaStock.repository.FornecedorRepository;
+import com.umprogramax.lojaStock.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class FuncionarioService {
+
+    @Autowired
+    FuncionarioRepository repository;
+
+    public Funcionario create(Funcionario obj) {
+        obj.getNome();
+        obj.getCpf();
+        obj.getRegistro();
+        obj.getTipo();
+        obj.getEndereco();
+        return repository.save(obj);
+    }
+
+    public List<Funcionario> list() {
+        return (List<Funcionario>) repository.findAll();
+    }
+
+    public Funcionario update(Funcionario funcionario) {
+        return repository.save(funcionario);
+    }
+
+    public void delete(UUID id) {
+        repository.deleteById(id);
+    }
+
+    public Funcionario charge(UUID id) {
+        return (Funcionario) repository.findById(id).get();
+    }
+
 }
