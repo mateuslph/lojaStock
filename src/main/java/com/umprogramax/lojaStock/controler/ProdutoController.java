59effc937da72af01005dd91e01e7d79450c2054
mateuslph <mlptche@gmail.com>
2024-01-27 21:35:06 -0300

    rota produto concluida

--- src/main/java/com/umprogramax/lojaStock/controler/ProdutoController.java
+++ src/main/java/com/umprogramax/lojaStock/controler/ProdutoController.java
@@ -1,7 +1,46 @@
 package com.umprogramax.lojaStock.controler;
 
+import com.umprogramax.lojaStock.model.Funcionario;
+import com.umprogramax.lojaStock.model.Produto;
+import com.umprogramax.lojaStock.service.FuncionarioService;
+import com.umprogramax.lojaStock.service.ProdutoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 public class ProdutoController {
+
+    private ProdutoService service;
+
+    @PostMapping(value = "/salvar-produto")
+    public String save(@ModelAttribute("produto")Produto produto) {
+        service.create(produto);
+        return "redirect:/salvar-produto/index";
+    }
+
+    @GetMapping(value="/listar-produto")
+    public String index(Model model) {
+        model.addAttribute("produto", service.list());
+        return "/listar-produtos/index";
+    }
+
+    @PutMapping(value="/atualizar-produtos/{id}")
+    public String update(@PathVariable UUID id, @ModelAttribute("produtos") Produto produto, Model model) {
+        Produto obj = service.charge(id);
+        obj.setId(String.valueOf(id));
+        obj.setDescricao(produto.getDescricao());
+        obj.setPreco(produto.getPreco());
+        obj.setObservacao(produto.getObservacao());
+        service.update(obj);
+        return "redirect:/atualizar-produto/index";
+    }
+
+    public String delete(@PathVariable UUID id) {
+        service.delete(id);
+        return "redirect:/listar-produto/index";
+    }
+
 }
--- src/main/java/com/umprogramax/lojaStock/service/ProdutoService.java
+++ src/main/java/com/umprogramax/lojaStock/service/ProdutoService.java
@@ -1,7 +1,42 @@
 package com.umprogramax.lojaStock.service;
 
+import com.umprogramax.lojaStock.model.Funcionario;
+import com.umprogramax.lojaStock.model.Produto;
+import com.umprogramax.lojaStock.repository.FuncionarioRepository;
+import com.umprogramax.lojaStock.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository repository;
+
+    public Produto create(Produto obj) {
+        obj.getDescricao();
+        obj.getPreco();
+        obj.getObservacao();
+        return repository.save(obj);
+    }
+
+    public List<Produto> list() {
+        return (List<Produto>) repository.findAll();
+    }
+
+    public Produto update(Produto produto) {
+        return repository.save(produto);
+    }
+
+    public void delete(UUID id) {
+        repository.deleteById(id);
+    }
+
+    public Produto charge(UUID id) {
+        return (Produto) repository.findById(id).get();
+    }
+
 }
